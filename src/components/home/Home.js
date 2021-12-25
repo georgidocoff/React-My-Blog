72e77a6c5518b07dbc9e0a2b79b255ffcc5b632d
Georgi D <georgy_dkv@yahoo.com>
2021-12-25 17:08:18 +0200

    update functionality

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,39 +1,73 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
 import { Card, CardGroup } from "react-bootstrap";
 
-import {getThree} from '../../services/articlesService';
-function Home(){
- const[data, setDate]= useState([]);
+import { isAuth } from "../../services/authService";
+import { getUserById } from "../../services/userService";
+import { getThree } from "../../services/articlesService";
+import { UserContext, userContextValues } from "../../context/userContext";
 
- useEffect(()=>{
-   getThree().then((res)=>{
+function publishPostTime(createTime) {
+  return Math.round((Date.now() - Date.parse(createTime)) / (60 * 1000)) - 60;
+}
+
+function Home() {
+  const [data, setDate] = useState([]);
+  const [users, setUsers] = useState([]);
+
+  const userContext = useContext(UserContext);
+
+  function renderPublishUser(userId) {
+    let user = users?.find((x) => x.id == userId);
+    if (!user) {
+      return "N/A";
+    }
+    return `${user?.name} ${user?.surname}`;
+  }
+
+  useEffect(() => {
+    getThree()
+      .then((res) => {
         //console.log(res.result.items);
-        setDate(res.result.items);
-    })
-    .catch((err)=>{console.log(err)})
- },[])
-    
- console.log(data);
+        setDate(res?.result.items);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
+  useEffect(()=>{
+    userContext.getAll().then((res) => {
+      //console.log(res.result);
+      setUsers(res?.result.items);
+    }).catch((err) => { console.log(err) });
+  },[userContext])
+
+  data.map((x) => (x.userFullName = renderPublishUser(x.creatorUserId)));
   return (
     <CardGroup>
-      {data.map((x)=>{
-        return(
-        <Card>
-        <Card.Img variant="top" src="" />
-        <Card.Body>
-          <Card.Title>{x.title}</Card.Title>
-          <Card.Text>
-            {x.description}
-          </Card.Text>
-        </Card.Body>
-        <Card.Footer>
-          <small className="text-muted">Last updated {Math.round((Date.now() - Date.parse(x.creationTime))/(60*1000))-60} mins ago</small>
-        </Card.Footer>
-      </Card>)
-      })}no
+      {(data?.length > 0 &&
+        data.map((x) => {
+          return (
+            <Card key={x.id}>
+              <Card.Img variant="top" src="" />
+              <Card.Body>
+                <Card.Title>{x.title}</Card.Title>
+                <Card.Text>{x.description}</Card.Text>
+              </Card.Body>
+              <Card.Footer>
+                <small className="text-muted">
+                  Created by: {x.userFullName}
+                </small>
+                <br />
+                <small className="text-muted">
+                  Post updated {publishPostTime(x.creationTime)} mins ago
+                </small>
+              </Card.Footer>
+            </Card>
+          );
+        })) || <h5>No data avaiable...</h5>}
     </CardGroup>
   );
-};
+}
 
 export default Home;
